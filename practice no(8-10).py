243c86430c7d1d0acccdd550fcd6bd417de053f1
Opu-Gosh <opugosh80@gmail.com>
2026-07-10 00:22:15 +0600

    Add files via upload

---
+++ practice no(8-10).py
@@ -0,0 +1,35 @@
+# problem No 8.
+num1 = int(input("Enter the number:"))
+num2 = int(input("Enter the number:"))
+sum = num1 + num2
+print(sum)
+#sortcut
+print("The sum is:", int(input("Enter the number:")) + int(input("Enter the number:")))
+
+#problem No 9.
+num3 = int(input("Enter the number:"))
+num4 = int(input("Enter the number:"))
+difference = num3 - num4
+print("The difference is:",difference)
+
+#sortcut
+print("The difference is:", int(input("Enter the number:")) - int(input("Enter the number:")))
+
+#Problem No 10.
+num5 = float(input("Enter the number:"))
+num6 = float(input("Enter the number:"))
+product = num5 * num6
+print("Your ans. is:",product)
+
+#sortcut
+print("product:", float(input("Enter the number:")) * float(input("Enter the number:")))
+#practice.
+note_book = float(input("prize:"))
+I_want = float(input("you need:"))
+cal = note_book*I_want
+print(cal)
+
+
+
+
+
