4da11fe872752627a7aabbb0cb6d3cf237619473
Opu-Gosh <opugosh80@gmail.com>
2026-07-09 14:51:18 +0600

    Add files via upload

---
+++ practice (1-7).py
@@ -0,0 +1,21 @@
+#Basic Output.
+print("Hello word.")
+print("Opu Gosh.")
+
+print("House No: 37,Road No: 01\n Gritupulli, Cox's bazar, Bangladesh.")
+
+print("Name	: Opu Gosh.\nRoll	: 358430.\nAge	: 17.\nDistrict: Cox's Bazar.")
+
+print("Course\t: Python Programming.\nTopic\t: Escape Characters.\nstatus\t: \"Completed\"\npath\t: c: users\\python\\Note.")
+
+print(30,58,86,45,56,46,34, sep=":")
+print(10,32,sep=":")
+print("Mango","Lichi","Plam",sep="-")
+
+print("I have",end=" ")
+print("a pen.")
+print("I have no",end=" ")
+print("money.")
+print("Because,",end=" ")
+print("I am a poor.")
+
