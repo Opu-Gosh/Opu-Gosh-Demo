35862aa22f66dbe428d561caaec98df4664a1f35
Opu-Gosh <opugosh80@gmail.com>
2026-07-12 14:44:00 +0600

    Add files via upload

---
+++ practice (21-23).py
@@ -0,0 +1,37 @@
+#practice no. 21
+a = int(input("value of a:"))
+b = int(input("value of b:"))
+print("Addition(a+b):",(a+b))
+print("subtraction(a-b):",(a-b))
+print("Multipication(a*b):",(a*b))
+print("Divition(a/b):",(a/b))
+print("Remainder(a%b):",(a%b))
+print("Floor Divition(a//b):",(a//b))
+print("Power(a**b):",(a**b))
+
+#practice no. 22
+num = int(input("Enter your number:"))
+num += 57
+print("After cal.(num += 57):",num)
+num -= 50
+print("After cal.(num -= 50):",num)
+num *= 3
+print("After cal.(num *= 3):",num)
+num /= 5
+print("After cal.(num /= 5):",num)
+
+#practice no.23
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+print("x is equal to y:",x == y)
+print("x is not equal to y:",x != y)
+print("(x<y):",x<y)
+print("(x>y):",x>y)
+print("(x <= y):",x <= y)
+print("(x >= y):",x >= y)
+
+
+
+
+
+
