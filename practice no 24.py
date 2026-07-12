07cf8d0e4aeee4c7f7b7805ea5d2c0d2788ce625
Opu-Gosh <opugosh80@gmail.com>
2026-07-12 14:44:46 +0600

    Add files via upload

---
+++ practice no 24.py
@@ -0,0 +1,12 @@
+# practice no. 24
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+
+# And Operator.
+print("(x<y) and (x>y):",(x<y) and (x>y))
+#OR Operator.
+print("(x<y) or (x>y):",(x<y) or (x>y))
+#Not Operator.
+print("Not(x<y):", not (x<y))
+
+	
