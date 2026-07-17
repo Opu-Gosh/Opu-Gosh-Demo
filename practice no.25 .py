ede392a5528ed46e22a6cdbd57a662b1ff36d39e
Opu-Gosh <opugosh80@gmail.com>
2026-07-17 16:06:48 +0600

    Add files via upload

---
+++ practice no.25 .py
@@ -0,0 +1,8 @@
+# practice no. 25
+list1 = [10,46,56,78]
+list2 = [10,46,56,78]
+list3 = list1
+print("list1 == list2 :", list1 == list2)
+print("list1 is list2 :", list1 is list2)
+print("list1 is list3 :", list1 is list3)
+print("list1 is not list2 :", list1 is not list2)
