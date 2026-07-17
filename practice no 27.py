47cd708eb2a1c10f0ee8f1e0871e027782b1da3f
Opu-Gosh <opugosh80@gmail.com>
2026-07-17 17:22:13 +0600

    Add files via upload

---
+++ practice no 27.py
@@ -0,0 +1,10 @@
+# practice no. 27
+a = int(input("Enter your 1st intezer value: "))
+b = int(input("Enter your 2st intezer value: "))
+
+print("Bitwise And (a & b):", (a & b))
+print("Bitwise OR (a | b):", (a | b))
+print("Bitwise XOR (a^b):",(a^b))
+print("Bitwise Not (~a):",(~a))
+print("Bitwise << (a << 2):",(a << 2))
+print("Bitwise >> (b >> 2):",(b >> 2))
