3fa4236b32884f864dcf88e14e7428bedee1fa7b
Opu-Gosh <opugosh80@gmail.com>
2026-07-17 16:07:17 +0600

    Add files via upload

---
+++ practice no.26.py
@@ -0,0 +1,9 @@
+# practice no. 26
+vowel =["a","e","i","o","u"]
+text = "I am learning python programming. "
+
+print("is i in vowel: ","i" in vowel)
+print("is s in vowel: ","s" in vowel)
+print("is program in text: ","program" in text)
+print("is java in text:","java" in text)
+
